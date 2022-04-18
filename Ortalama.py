618cf368dd0a5a076f23a382a3240f7cbf47194f
Mehmet Nuri ÖZTÜRK <mnozturk@outlook.com.tr>
2022-04-18 21:43:44 +0300

    Properties Iterator ve Generatorler

---
+++ Ortalama.py
@@ -0,0 +1,8 @@
+## SORU: Vize ve final notunu alarak ortalama
+#  hesaplayan(Ortalama ()) univeriste isminde bir sınıf tanımlayınız.
+from Universite import Universite
+
+uni =  Universite()
+uni.vize = 102
+uni.final = 900
+uni.ortlama()
